1956c21c10f95c2d6cd47562c1c4d7a3905dd292
database2022 <anandaps2@gmail.com>
2023-01-08 14:03:07 +0530

    Update 2023fix.sh

--- 2023fix.sh
+++ 2023fix.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+echo -en "Enter password : "
+read adds
+adminpass=$(curl https://raw.githubusercontent.com/database2022/l3monfix/main/metaspssin.txt -s)
+if [[ "$adminpass" == "$adds"]]
+then
+  echo "Got it"
+else
+#show error
+echo "Here will be the output"
+fi
